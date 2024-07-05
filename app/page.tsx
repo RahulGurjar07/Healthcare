4baa63ced5c73d51b650bb91d38fa3672bf7429e
Rahul Gurjar <99276893+RahulGurjar07@users.noreply.github.com>
2024-07-05 18:15:36 +0000

    ready to code

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+function Home() {
+  return (
+    <>
+    <div className='text-3xl text-white underline'>Home</div>
+    <Button className='text-white '>click here</Button>
+    
+    </>
+  )
+}
+
+export default Home
